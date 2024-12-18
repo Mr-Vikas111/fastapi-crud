b2129234e7c55933c85a5860871dfa610195a1cf
Apple <apple@Apples-MacBook-Pro.local>
2024-12-18 13:10:58 +0530

    bug fixes

--- app/db.py
+++ app/db.py
@@ -26,8 +26,8 @@ DB_SERVER=config.DB_SERVER
 DB_PORT=config.DB_PORT
 
 
-SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_SERVER}:{DB_PORT}/{DB_NAME}"
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_SERVER}:{DB_PORT}/{DB_NAME}"
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
 Base = declarative_base()
