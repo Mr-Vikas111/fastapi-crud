8b9b42a9c60c3be8d64ca124188dcd995cea292e
Apple <apple@Apples-MacBook-Pro.local>
2024-12-18 22:40:27 +0530

    test cases check

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 import sys
 import os
 
-# sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
 from fastapi import FastAPI
 import apis
 
