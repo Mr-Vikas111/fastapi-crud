a08f9d026103bf551d79f19bee9d9a2840bd1459
Apple <apple@Apples-MacBook-Pro.local>
2024-12-19 16:10:59 +0530

    response handling in swagger-doc

--- app/apis.py
+++ app/apis.py
@@ -7,24 +7,30 @@ from fastapi import APIRouter
 from schemas.user_schemas import *
 from fastapi.exceptions import HTTPException
 from dbconfig import db
+from typing import  Union
 
 
 
 router = APIRouter()
 
 # create user detail information
-@router.post("/user/create/")
+@router.post("/user/create/",response_model=UserRequest)
 async def create_new_user(payload:  UserRequest):
     try:
         users_ref = db.collection("users")
         request_payload = payload.dict()
-        doc_ref,data_ref = users_ref.add(request_payload)
-        return {"message": "User created successfully!","user_id":data_ref.id}
+        _,data_ref = users_ref.add(request_payload)
+        if data_ref:
+            saved_data = {**request_payload, "id": data_ref.id}
+            return {"message": "User created successfully!","response":saved_data}
+        else:
+            return {"message": "Failed","response":{}}
+            
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 # retrive user detail information
-@router.get("/user/detail/{user_id}")
+@router.get("/user/detail/{user_id}",response_model=UserRequest)
 def get_user_by_id(user_id: str):
     """Fetch a user by their document ID from Firestore."""
     user_ref = db.collection("users").document(user_id)
@@ -45,7 +51,7 @@ def get_all_users():
     return user_list
 
 # Update user information in Firestore
-@router.patch("/user/update/{user_id}")
+@router.patch("/user/update/{user_id}",response_model=UserRequest)
 async def update_user(user_id: str, payload: UserRequest):
     try:
         # Reference to the user document
@@ -93,4 +99,22 @@ async def update_user(user_id: str, payload: UserRequest):
     except Exception as e:
         print(f"Error occurred while updating user: {e}")
         raise HTTPException(status_code=500, detail="Internal server error")
-    
+
+
+
+@router.delete("/user/delete/{user_id}",response_model=DeleteResponse)
+async def delete_user(user_id: str):
+    try:
+        user_ref = db.collection("users").document(user_id)
+
+        doc = user_ref.get()
+
+        if not doc.exists:
+            raise HTTPException(status_code=404, detail="User not found")
+
+        user_ref.delete()
+        return {"message": "User deleted successfully", "user_id": user_id}
+
+    except Exception as e:
+        # If any error occurs, raise an internal server error
+        raise HTTPException(status_code=500, detail=str(e))
--- app/schemas/user_schemas.py
+++ app/schemas/user_schemas.py
@@ -21,10 +21,19 @@ class ProjectOne(BaseModel):
         pattern=r"^[A-Za-z\d@$!%*?&]{8,}$",
         description="Password must be at least 8 characters long and may include letters, numbers, and special characters @$!%*?&."
     )
-
-# class HashTag:
-#     def __init__(self, name: str):
-#         self.name = name
+    
+    
+    class Config:
+        schema_extra = {
+            "example": {
+                "project_id": 1,
+                "company_name": "Example Corp",
+                "first_name": "John",
+                "last_name": "Doe",
+                "email": "john.doe@example.com",
+                "password": "secureP@ss123"
+            }
+        }
 
 
 class ProjectTwo(BaseModel):
@@ -42,6 +51,20 @@ class ProjectTwo(BaseModel):
     
     class Config:
         arbitrary_types_allowed = True
+        schema_extra = {
+            "example": {
+                "project_id": 2,
+                "mobile": "+1234567890",
+                "first_name": "John",
+                "last_name": "Doe",
+                "hashtags": [
+                    {"name": "hashtag1"},
+                    {"name": "hashtag2"}
+                ]
+            }
+        }
+        
+        
 
 def convert_to_datetime(dob: date) -> datetime:
     return datetime(dob.year, dob.month, dob.day)
@@ -64,6 +87,48 @@ class ProjectThree(BaseModel):
     def validate_and_convert_dob(cls, v):
         # Convert datetime.date to datetime.datetime for Firestore compatibility
         return convert_to_datetime(v)
+    
+    class Config:
+        schema_extra = {
+            "example": {
+                "project_id": 3,
+                "mobile": "+19876543210",
+                "first_name": "Mike",
+                "last_name": "Smith",
+                "dob": "1990-01-01"
+            }
+        }
+    
+    
 
 class UserRequest(BaseModel):
     project: Union[ProjectOne, ProjectTwo, ProjectThree]
+    
+    class Config:
+        schema_extra = {
+            "example": {
+                "project": {
+                    "project_id": 1,
+                    "company_name": "Example Corp",
+                    "first_name": "John",
+                    "last_name": "Doe",
+                    "email": "john.doe@example.com",
+                    "password": "secureP@ss123"
+                },
+                "project": {
+                    "project_id": 2,
+                    "mobile": "+1234567890",
+                    "first_name": "John",
+                    "last_name": "Doe",
+                    "hashtags": [
+                    {"name": "hashtag1"},
+                    {"name": "hashtag2"}
+                ]   
+                }
+            }
+        }
+
+class DeleteResponse(BaseModel):
+    message:str = Field(example="User deleted successfully")
+    user_id :str = Field(example="Z2fbhgU9cc5DKpcMKMr6")
+    
